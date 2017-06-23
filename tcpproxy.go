af97cdd9d95a6cae6a52775ab8d5b3fc456a6817
Brad Fitzpatrick <brad@danga.com>
2017-06-22 18:11:58 -0700

    Fix copy/paste-o in doc example.

--- tcpproxy.go
+++ tcpproxy.go
@@ -12,8 +12,8 @@
 //     p.AddHTTPHostRoute(":80", "foo.com", tcpproxy.To("10.0.0.1:8081"))
 //     p.AddHTTPHostRoute(":80", "bar.com", tcpproxy.To("10.0.0.2:8082"))
 //     p.AddRoute(":80", tcpproxy.To("10.0.0.1:8081")) // fallback
-//     p.AddSNIHostRoute(":443", "foo.com", tcpproxy.To("10.0.0.1:4431"))
-//     p.AddSNIHostRoute(":443", "bar.com", tcpproxy.To("10.0.0.2:4432"))
+//     p.AddSNIRoute(":443", "foo.com", tcpproxy.To("10.0.0.1:4431"))
+//     p.AddSNIRoute(":443", "bar.com", tcpproxy.To("10.0.0.2:4432"))
 //     p.AddRoute(":443", tcpproxy.To("10.0.0.1:4431")) // fallback
 //     log.Fatal(p.Run())
 //
