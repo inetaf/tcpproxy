4e04b92f29ea8f8a10d28528a47ecc0f93814473
Brad Fitzpatrick <brad@danga.com>
2022-10-16 16:53:59 -0700

    gofmt for Go 1.19

--- tcpproxy.go
+++ tcpproxy.go
@@ -18,14 +18,14 @@
 //
 // Typical usage:
 //
-//     var p tcpproxy.Proxy
-//     p.AddHTTPHostRoute(":80", "foo.com", tcpproxy.To("10.0.0.1:8081"))
-//     p.AddHTTPHostRoute(":80", "bar.com", tcpproxy.To("10.0.0.2:8082"))
-//     p.AddRoute(":80", tcpproxy.To("10.0.0.1:8081")) // fallback
-//     p.AddSNIRoute(":443", "foo.com", tcpproxy.To("10.0.0.1:4431"))
-//     p.AddSNIRoute(":443", "bar.com", tcpproxy.To("10.0.0.2:4432"))
-//     p.AddRoute(":443", tcpproxy.To("10.0.0.1:4431")) // fallback
-//     log.Fatal(p.Run())
+//	var p tcpproxy.Proxy
+//	p.AddHTTPHostRoute(":80", "foo.com", tcpproxy.To("10.0.0.1:8081"))
+//	p.AddHTTPHostRoute(":80", "bar.com", tcpproxy.To("10.0.0.2:8082"))
+//	p.AddRoute(":80", tcpproxy.To("10.0.0.1:8081")) // fallback
+//	p.AddSNIRoute(":443", "foo.com", tcpproxy.To("10.0.0.1:4431"))
+//	p.AddSNIRoute(":443", "bar.com", tcpproxy.To("10.0.0.2:4432"))
+//	p.AddRoute(":443", tcpproxy.To("10.0.0.1:4431")) // fallback
+//	log.Fatal(p.Run())
 //
 // Calling Run (or Start) on a proxy also starts all the necessary
 // listeners.
